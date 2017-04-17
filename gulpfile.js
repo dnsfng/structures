c667676cd07c9611cc9f15c44e1cd14f8380b9b1
dnsfng <denis@fongue.com>
2017-04-17 20:32:40 +0200

    Add SASS error support

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,21 @@ require('es6-promise').polyfill();
 var gulp          = require('gulp');
 var sass          = require('gulp-sass');
 var autoprefixer  = require('gulp-autoprefixer');
+var plumber       = require('gulp-plumber');
+var gutil         = require('gulp-util');
+
+var onError = function (err) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
 
 // SASS task
 gulp.task('sass', function(){
   return gulp.src('./sass/**/*.scss')
   .pipe(sass())
   .pipe(autoprefixer())
+  .pipe(plumber({ errorHandler: onError }))
   .pipe(gulp.dest('./'))
 });
 
