0df5619ee849c3509e43c0fba1ad9eba029c055c
dnsfng <denis@fongue.com>
2017-04-24 20:08:33 +0200

    Remove sidebar on all part of theme

--- functions.php
+++ functions.php
@@ -160,18 +160,20 @@ add_action( 'after_setup_theme', 'structures_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function structures_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'structures' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'structures' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'structures_widgets_init' );
+
+// TODO: remove sidebar support and widget area
+// function structures_widgets_init() {
+// 	register_sidebar( array(
+// 		'name'          => esc_html__( 'Sidebar', 'structures' ),
+// 		'id'            => 'sidebar-1',
+// 		'description'   => esc_html__( 'Add widgets here.', 'structures' ),
+// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</section>',
+// 		'before_title'  => '<h2 class="widget-title">',
+// 		'after_title'   => '</h2>',
+// 	) );
+// }
+// add_action( 'widgets_init', 'structures_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
