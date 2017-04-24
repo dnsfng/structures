71704944315e77c3070510c8a177544e426206db
dnsfng <denis@fongue.com>
2017-04-24 18:14:41 +0200

    Remove footer metadata

--- footer.php
+++ footer.php
@@ -12,14 +12,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'structures' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'structures' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'structures' ), 'structures', '<a href="https://automattic.com/" rel="designer">dnsfng</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
