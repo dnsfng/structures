ba21149082432c2f9226fef3dcb7931e5dbd728e
dnsfng <denis@fongue.com>
2017-08-16 17:34:52 +0200

    Menu & Page : Add subpage menu support and style
    • Add Secondary Menu : shortcode, walker and output
    • Better style for both menus

--- functions.php
+++ functions.php
@@ -67,7 +67,91 @@ function structures_setup() {
 	        return $content;
 	    }
 	}
-	add_filter( 'the_content', 'strucutures_automatic_nbsp' );
+
+	// Add childpage display shortcode and add list structure (ul)
+	add_shortcode( 'list_children', 'structures_list_child_posts' );
+	function structures_list_child_posts( $atts ) {
+    global $id;
+		add_filter( 'wp_list_pages', 'structures_list_child_posts_output' );
+    wp_list_pages( array(
+        'title_li'    => '',
+        'child_of'    => $id,
+        'sort_column' => 'menu_order',
+				'walker'			=> new Subpages_list_Walker()
+    ) );
+	}
+
+	function structures_list_child_posts_output($output) {
+		$output_before =  "<nav id='subpage-navigation' class='secondary-navigation'> <div class='menu secondary-menu'><ul class='subpage-nav-menu'>";
+		$output_after =  "</ul></div></nav>";
+
+		$output = $output_before.$output.$output_after ;
+
+		return $output;
+	}
+
+	/**
+	 * Main navigation custom walker
+	 */
+	 class Subpages_list_Walker extends Walker_Page {
+
+		 function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+
+	 		if ( isset( $args['item_spacing'] ) && 'preserve' === $args['item_spacing'] ) {
+	 			$t = "\t";
+	 			$n = "\n";
+	 		} else {
+	 			$t = '';
+	 			$n = '';
+	 		}
+
+	 		if ( $depth ) {
+	 			$indent = str_repeat( $t, $depth );
+	 		} else {
+	 			$indent = '';
+	 		}
+
+	 		$css_class = array( 'subpage-item', 'subpage-item-' . $page->ID );
+
+	 		if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
+	 			$css_class[] = 'page_item_has_children';
+	 		}
+
+	 		if ( ! empty( $current_page ) ) {
+	 			$_current_page = get_post( $current_page );
+	 			if ( $_current_page && in_array( $page->ID, $_current_page->ancestors ) ) {
+	 				$css_class[] = 'current_page_ancestor';
+	 			}
+	 			if ( $page->ID == $current_page ) {
+	 				$css_class[] = 'current_page_item';
+	 			} elseif ( $_current_page && $page->ID == $_current_page->post_parent ) {
+	 				$css_class[] = 'current_page_parent';
+	 			}
+	 		} elseif ( $page->ID == get_option('page_for_posts') ) {
+	 			$css_class[] = 'current_page_parent';
+	 		}
+
+	 		$css_classes = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+	 		if ( '' === $page->post_title ) {
+	 			/* translators: %d: ID of a post */
+	 			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+	 		}
+
+	 		$args['link_before'] = empty( $args['link_before'] ) ? '' : $args['link_before'];
+	 		$args['link_after'] = empty( $args['link_after'] ) ? '' : $args['link_after'];
+
+	 		$output .= $indent . sprintf(
+	 			'<li class="%s"><a href="%s">%s%s%s</a>',
+	 			$css_classes,
+	 			get_permalink( $page->ID ),
+	 			$args['link_before'],
+	 			apply_filters( 'the_title', $page->post_title, $page->ID ),
+	 			$args['link_after']
+	 		);
+
+	 	}
+	 }
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
