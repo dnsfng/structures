c175c4e2dec72f47fdfc233919f996ec0433fbc5
dnsfng <denis@fongue.com>
2017-11-12 11:07:03 +0100

    Fix production bug
    - New Wordpress version 4.8.3 manage image widget differently and
    without plugin
    - Fix third navigation height regarding different content

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'structures' ); ?></a>
 
-	<?php if ( $post->post_parent ) { ?>
+	<?php if ( !has_children() ) { ?>
 
 	<header id="masthead" class="site-header" role="banner">
 
