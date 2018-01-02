0d9fe48429598f1d0104ba4c2760c635f3293dc7
dnsfng <denis@fongue.com>
2018-01-02 18:36:10 +0100

    Add Google Anaytics

--- functions.php
+++ functions.php
@@ -482,9 +482,25 @@ function structures_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
 }
 add_action( 'wp_enqueue_scripts', 'structures_scripts' );
 
+// adds google analytics code to footer
+function add_google_analytics() {
+echo "
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-77478404-1', 'auto');
+	ga('send', 'pageview');
+</script>
+";
+}
+add_action('wp_footer', 'add_google_analytics');
 
 /**
  * Remove useless support
